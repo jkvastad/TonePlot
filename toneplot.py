8dca4dd7904a57510c88b01906aac37a3990a7c9
jkvastad <jkvastad@gmail.com>
2022-07-05 13:35:58 +0200

    Added undertone plotting with relative intensities for overlaps

--- toneplot.py
+++ toneplot.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 from fractions import Fraction
 import matplotlib.pyplot as plt
@@ -5,12 +6,14 @@ from math import lcm
 from math import gcd
 from itertools import combinations
 
+from matplotlib.patches import Rectangle
+
 C_4 = 261.6256
 TWELVE_TET_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 
-def get_closest_scientific_pitch(fraction, fundamental_frequency=C_4):
-    fraction_as_pitch = fraction * fundamental_frequency
+def get_closest_scientific_pitch(fraction):
+    fraction_as_pitch = fraction * C_4
     smallest_diff = 100000
     name_number = '-'
     for n in range(-48, 60):
@@ -20,7 +23,9 @@ def get_closest_scientific_pitch(fraction, fundamental_frequency=C_4):
             smallest_diff = current_diff
             name_number = n
     pitch_name = TWELVE_TET_NAMES[name_number % 12] + f'{4 + name_number // 12}'
-    return fraction, pitch_name, smallest_diff / (C_4 * 2 ** (name_number / 12)), smallest_diff
+    closest_pitch = C_4 * 2 ** (name_number / 12)
+    return fraction, pitch_name, 1200 * math.log2(
+        fraction_as_pitch / closest_pitch), smallest_diff / closest_pitch, smallest_diff
 
 
 def get_lcm_for_fractions(*fractions: Fraction):
@@ -73,6 +78,14 @@ def get_widths_by_fractions(unique_fractions):
 
 def get_undertones_for_harmonic(harmonic, number_of_undertones):
     return [Fraction(harmonic, i) for i in range(1, number_of_undertones + 1)]
+
+
+def get_overtones_for_fraction(fraction: Fraction, overtones: int = 0) -> list[Fraction, ...]:
+    return [fraction + i * fraction for i in range(overtones + 1)]
+
+
+def get_overtones_for_fractions(overtones: int = 0, *fractions: Fraction) -> set[Fraction, ...]:
+    return {overtone for fraction in fractions for overtone in get_overtones_for_fraction(fraction, overtones)}
 
 
 def get_octave_reduction(fraction, octave=2):
@@ -131,17 +144,18 @@ def plot_notes_with_dissonance_less_than(max_dissonance):
     plt.show()
 
 
-def plot_wavelength_multiples_for_fraction_sets(fraction_sets: list[set[Fraction, ...], ...], lcm_plot=False,
-                                                title=None, label_style=None):
+def plot_wavelength_multiples_for_fraction_sets(fraction_sets: list[set[Fraction, ...], ...], lcm_plot=True,
+                                                title=None, label_style='None', plot_style='None'):
     fig, axs = plt.subplots(len(fraction_sets), 1, layout='constrained', squeeze=False)
     for i in range(len(fraction_sets)):
-        plot_wavelength_multiples_for_fractions(axs[i][0], fraction_sets[i], lcm_plot, title, label_style)
+        plot_wavelength_multiples_for_fractions(axs[i][0], fraction_sets[i], lcm_plot, title, label_style, plot_style)
 
 
-def plot_wavelength_multiples_for_fractions(ax, fractions, lcm_plot, title, label_style):
+def plot_wavelength_multiples_for_fractions(ax, fractions, lcm_plot, title, label_style, plot_style):
     wavelengths = sorted([Fraction(fraction.denominator, fraction.numerator) for fraction in fractions], reverse=True)
     global_wavelength_lcm = get_lcm_for_fractions(*wavelengths)
     all_multiples = set()
+    multiples_for_wavelength = {}
 
     for wavelength in wavelengths:
         if lcm_plot:
@@ -150,18 +164,31 @@ def plot_wavelength_multiples_for_fractions(ax, fractions, lcm_plot, title, labe
             multiples = wavelength.denominator
 
         current_wavelengths = [wavelength * i for i in range(1, multiples + 1)]
+        multiples_for_wavelength[wavelength] = current_wavelengths
         all_multiples.update(current_wavelengths)
 
+    if plot_style == 'compact':
+        new_multiples_for_wavelength = {}
+        old_multiples_for_wavelength = {key: value for key, value in multiples_for_wavelength.items()}
+        for wavelength, multiples in multiples_for_wavelength.items():
+            del old_multiples_for_wavelength[wavelength]
+            for old_multiples in old_multiples_for_wavelength.values():
+                if set(multiples).issubset(set(old_multiples)):
+                    break
+            else:
+                new_multiples_for_wavelength[wavelength] = multiples
+
+        multiples_for_wavelength = new_multiples_for_wavelength
+
+    for wavelength in multiples_for_wavelength.keys():
+        current_wavelengths = multiples_for_wavelength[wavelength]
         bar_heights = [wavelength] * len(current_wavelengths)
         bar_widths = [-wavelength] * len(current_wavelengths)
         ax.bar(current_wavelengths, bar_heights, bar_widths, align='edge', **{'edgecolor': 'black', 'linewidth': 0.5})
 
-    if label_style[:3] == 'lcm':
+    if label_style == 'lcm':
         lcm_for_combinations = get_lcm_for_combinations(set(wavelengths))
         lcm_multiples = set()
-        if label_style == 'lcm_print':
-            for combination_lcm in sorted(lcm_for_combinations):
-                print(combination_lcm)
         for lcm_for_combination in lcm_for_combinations:
             multiples = int(global_wavelength_lcm * lcm_for_combination.denominator / lcm_for_combination.numerator)
             lcm_multiples.update([lcm_for_combination * i for i in range(1, multiples + 1)])
@@ -171,6 +198,27 @@ def plot_wavelength_multiples_for_fractions(ax, fractions, lcm_plot, title, labe
     else:
         x_ticks = sorted(all_multiples) + [0]
     y_ticks = sorted(wavelengths) + [Fraction(0), Fraction(1)]
+
+    pitch_classes = set()
+    for tick in sorted(x_ticks, key=lambda fraction: (fraction.denominator, - fraction.numerator)):
+        try:
+            scientific_pitch = get_closest_scientific_pitch(Fraction(tick.denominator, tick.numerator))
+            pitch_classes.add(scientific_pitch[1][:-1])
+            print(scientific_pitch)
+        except ZeroDivisionError:
+            pass
+    print(len(pitch_classes), sorted(pitch_classes))
+    print()
+    for wavelength in sorted(wavelengths):
+        try:
+            scientific_pitch = get_closest_scientific_pitch(Fraction(wavelength.denominator, wavelength.numerator))
+            pitch_classes.add(scientific_pitch[1][:-1])
+            print(scientific_pitch)
+        except ZeroDivisionError:
+            pass
+    print(len(pitch_classes), sorted(pitch_classes))
+    print('---')
+    print()
 
     ax.set_xticks(x_ticks, x_ticks)
     ax.set_yticks(y_ticks, y_ticks)
@@ -198,7 +246,8 @@ def temp():
     Bb_4_major = {Fraction(7, 4), Fraction(7 * 5, 4 * 4), Fraction(7 * 3, 4 * 2)}
     Bb_6_major = {Fraction(7, 1), Fraction(7 * 5, 1 * 4), Fraction(7 * 3, 1 * 2)}
 
-    # my_fractions = [C_4_major, C_4_major | C_5_major, C_4_major | C_5_major | G_5_major]
+    my_fractions = [C_4_major, C_4_major | C_5_major, C_4_major | C_6_major]
+    """
     my_fractions = [
         # C_4_major,
         # C_4_major | C_5_major,
@@ -208,24 +257,125 @@ def temp():
         # C_4_major | C_5_major | G_5_major | C_6_major | E_6_major | G_6_major,
         # C_4_major | C_5_major | G_5_major | C_6_major | E_6_major | G_6_major | A_sharp_6_major
     ]
+    
     my_inverted_fractions = [{Fraction(fraction.denominator, fraction.numerator) for fraction in fractions} for
                              fractions in my_fractions]
-    plot_wavelength_multiples_for_fraction_sets(my_fractions, lcm_plot=True, label_style='lcm_print')
-    # plot_wavelength_multiples_for_fraction_sets(my_inverted_fractions, lcm_plot=True)
+    """
+    # plot_wavelength_multiples_for_fraction_sets(my_fractions)
+    max_harmonic = 14
+    # TODO: Kolla maximalt konsonanta toner ("linjerna" i konsonansgrafen)
+    # TODO: färgintensitet på grafblocken beroende på resulterande amplitud? Alpha värden mot vit bakgrund?
+    my_fractions = [
+        # get_overtones_for_fractions(19, Fraction(1)),
+        # get_overtones_for_fractions(8, Fraction(1)),
+        get_overtones_for_fractions(8, Fraction(1)) | get_overtones_for_fractions(11, Fraction(2, 3)),
+        get_overtones_for_fractions(11, Fraction(1)) | get_overtones_for_fractions(8, Fraction(3, 2)),
+
+        # get_overtones_for_fractions(5, Fraction(1)) | get_overtones_for_fractions(8, Fraction(1, 2)),
+        # get_overtones_for_fractions(8, Fraction(1)) | get_overtones_for_fractions(3, Fraction(3, 2)),
+        # get_overtones_for_fractions(11, Fraction(1)) | get_overtones_for_fractions(9, Fraction(6, 5)),
+        # get_overtones_for_fractions(8, Fraction(1)) | get_overtones_for_fractions(8, Fraction(2, 3)),
+        # get_overtones_for_fractions(5, Fraction(1)) | get_overtones_for_fractions(6, Fraction(3, 4)),
+
+        # get_overtones_for_fractions(max_harmonic, Fraction(1), Fraction(3, 2)),
+        # get_overtones_for_fractions(max_harmonic, Fraction(1), Fraction(3)),
+        # get_overtones_for_fractions(max_harmonic, Fraction(1), Fraction(3, 2), Fraction(3)),
+        # get_overtones_for_fractions(15, Fraction(1)) | get_overtones_for_fractions(6, Fraction(3))
+    ]
+    for fractions in my_fractions:
+        plot_wavelength_multiples_for_fraction_sets([fractions], label_style='lcm')
+        plot_wavelength_multiples_for_fraction_sets([fractions], plot_style='compact')
+        # plot_wavelength_multiples_for_fraction_sets(my_inverted_fractions, lcm_plot=True)
 
 
-def plot_harmonic_undertones():
-    for harmonic in range(3, 7):
-        my_fractions = [{Fraction(i, j) for i in range(1, harmonic + 1) for j in range(1, i + 1)}]
-        plot_wavelength_multiples_for_fraction_sets(my_fractions, lcm_plot=True, title=f"Max harmonic {harmonic}")
+def plot_rectangles(ax, *rectangles: Rectangle):
+    for rectangle in rectangles:
+        ax.add_patch(rectangle)
+
+
+def plot_undertones(fractions: list[Fraction, ...], number_of_overtones: list[int, ...]):
+    if len(fractions) != len(number_of_overtones):
+        print(f"Number of fractions does not match number of overtones: {len(fractions)} != {len(number_of_overtones)}")
+        return
+
+    fig, ax = plt.subplots(layout='constrained')
+    original_wavelengths = sorted([Fraction(fraction.denominator, fraction.numerator) for fraction in fractions],
+                                  reverse=True)
+    global_wavelength_lcm = get_lcm_for_fractions(*original_wavelengths)
+
+    # init wavelengths_by_denominator
+    wavelengths_by_denominator = defaultdict(list)
+    for i in range(len(fractions)):
+        for overtone in get_overtones_for_fraction(fractions[i], number_of_overtones[i]):
+            # overtone numerator is corresponding wavelength's denominator
+            wavelengths_by_denominator[overtone.numerator].append(Fraction(overtone.denominator, overtone.numerator))
+
+    global_alpha = 1 / max([len(wavelengths) for wavelengths in wavelengths_by_denominator.values()])
+
+    # init empty rectangles
+    rectangles_by_wavelength_denominator = defaultdict(list)
+    current_y = 0
+    for denominator in sorted(wavelengths_by_denominator.keys(), reverse=True):
+        current_x = 0
+        rectangle_size = 1 / denominator
+        rectangle_params = {'edgecolor': 'black', 'linewidth': 0.5, 'facecolor': f"C{denominator - 1}"}
+        for i in range(int(global_wavelength_lcm * denominator)):
+            current_rectangle = Rectangle((current_x, current_y), rectangle_size, rectangle_size - current_y,
+                                          **rectangle_params)
+            current_rectangle.set_alpha(0)
+            rectangles_by_wavelength_denominator[denominator].append(current_rectangle)
+            current_x += rectangle_size
+        current_y = rectangle_size
+
+    # update wavelength intensity
+    for denominator, wavelengths in wavelengths_by_denominator.items():
+        for wavelength in wavelengths:
+            for multiple in range(1, int(global_wavelength_lcm * wavelength.denominator / wavelength.numerator) + 1):
+                current_rectangle = rectangles_by_wavelength_denominator[denominator][
+                    wavelength.numerator * multiple - 1]
+                current_rectangle.set_alpha(current_rectangle.get_alpha() + global_alpha)
+
+    # draw rectangles
+    for rectangles in rectangles_by_wavelength_denominator.values():
+        plot_rectangles(ax, *rectangles)
+
+    # set labels etc.
+    unique_wavelengths = {wavelength for wavelengths in wavelengths_by_denominator.values() for wavelength in
+                          wavelengths}
+    lcm_for_combinations = get_lcm_for_combinations(set(unique_wavelengths))
+    lcm_multiples = set()
+    for lcm_for_combination in lcm_for_combinations:
+        multiples = int(global_wavelength_lcm * lcm_for_combination.denominator / lcm_for_combination.numerator)
+        lcm_multiples.update([lcm_for_combination * i for i in range(1, multiples + 1)])
+
+    lcm_multiples.add(Fraction(0))
+    x_ticks = list(lcm_multiples)
+
+    unique_denominators = {Fraction(1, wavelength.denominator) for wavelength in unique_wavelengths}
+    y_ticks = sorted(unique_denominators) + [Fraction(0), Fraction(1)]
+
+    ax.set_xticks(x_ticks, x_ticks)
+    ax.set_yticks(y_ticks, y_ticks)
+
+    ax.set_xlabel('Wavelength')
+    ax.set_ylabel('Original Wavelength')
+    ax.margins(0)
 
 
 if __name__ == '__main__':
     # TODO: "naturliga" toner borde vara bra på att behålla lcm för ackord. Implicerar små primtalskonsonanser.
     # TODO: Hur ser lcm ut vid vanliga ackordövergångar? Virtuella ackord p.g.a. rytm?
     # TODO: Hur ser lcm ut vid vanliga melodier?
+    # TODO: Hur låter transendentala intervall, typ pi? Hur låter irrationella intervall, typ roten ur 2?
+
+    # TODO: Hur låter en grundton med många övertoner jämfört med en grundton ihop med ett rationellt tal?
+    #  T.ex. 1 med 9 övertonerjämfört med 3/2 med 3 övertoner och 1 med 3-6 övertoner samtidigt som 3/2 med 3?
+    #  Se kompakta grafer för idén
+    #  rationella toner ihop med grundton kanske hintar åt hjärnan att det är övertoner men inkompletta? Ger rörelse?
     # for i in range(1, 10):
     #    for j in range(1, 10):
     #        print(get_closest_scientific_pitch(Fraction(i, j)))
-    temp()
+    my_fractions = [Fraction(1), Fraction(3, 2)]
+    my_number_of_overtones = [3, 3]
+    plot_undertones(my_fractions, my_number_of_overtones)
     plt.show()
